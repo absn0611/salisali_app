bc01d0498a0295abcbdc9b48ff2857634becfeb8
absn0611 <51868578+absn0611@users.noreply.github.com>
2019-07-25 15:59:51 +0900

    commit4

--- salisali/config/routes.rb
+++ salisali/config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   delete  'logout',  to: 'sessions#destroy'
   resources :users
   post    'stocks',   to: 'stocks#show'
+  get    'stocks',   to: 'stocks#show'
+  post 'orders', to: 'orders#create'
+
   root 'stocks#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
