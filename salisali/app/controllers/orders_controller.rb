e1abf872fd74bdc0c8439d65d88bd10b3f914ca7
absn0611 <51868578+absn0611@users.noreply.github.com>
2019-07-28 12:22:07 +0900

    commit8

--- salisali/app/controllers/orders_controller.rb
+++ salisali/app/controllers/orders_controller.rb
@@ -10,14 +10,17 @@ class OrdersController < ApplicationController
         if  params[:amount]
             params[:user_id].count.times do |i|
                 if params[:amount][i] == 0 or params[:amount][i] == nil
-                      render plain: params.inspect
 
                 else
 
                     @order = Order.new(user_id:@current_user.id, goods_master_id:params[:goods_master_id][i], amount:params[:amount][i],delivery_date:Time.current.since(7.days))
                     @order.save
                 end
+                
             end
+
+            # render plain: params.inspect
+
         # redirect_to root_path
     end
 
