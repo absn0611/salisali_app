162e0c7dfd9deffcbd7955f7846f7abc77b840e4
absn0611 <51868578+absn0611@users.noreply.github.com>
2019-07-30 18:56:01 +0900

    commit10

--- salisali/app/controllers/orders_controller.rb
+++ salisali/app/controllers/orders_controller.rb
@@ -13,31 +13,37 @@ class OrdersController < ApplicationController
     end
 
     def create
-        if  params[:amount]
-            params[:goods_master_id].count.times do |i|
-                if params[:amount][i] == 0 or params[:amount][i] == nil
-                    redirect_to root_path, notice: '注文する商品を指定してください' 
- 
-                else
 
-                    @order = Order.new(user_id:@current_user.id, goods_master_id:params[:goods_master_id][i], amount:params[:amount][i],delivery_date:Time.current.since(7.days))
-                    # @order = Stock.create(goods_master_id:"J002-02",quantity_of_stock:"200")
-                    @order.save
-                    # @stock = Order.Stock.update(goods_master_id:params[:goods_master_id][i], quantity_of_stock:15)
-                    # redirect_to stock_path(@order.goods_master_id)
-                    # render plain: @order.stock.inspect
-                    # return
-                end
-                
+        if params[:amount].sum == 0 or params[:amount].sum == nil
+            redirect_to root_path, notice: '注文する商品を指定してください' 
+        else
+            params[:goods_master_id].count.times do |i| 
+                @order = Order.new(
+                    user_id:@current_user.id, 
+                    goods_master_id:params[:goods_master_id][i], 
+                    amount:params[:amount][i],
+                    delivery_date:Time.current.since(7.days)
+                    )
+                    
+                @order.save
+
+        end
+
+        params[:goods_master_id].count.times do |i| 
+            @stock = Stock.find(params[:goods_master_id][i])
+                @order = Stock.where(goods_master_id:params[:goods_master_id][i]).update(
+                    goods_master_id:params[:goods_master_id][i],
+                    quantity_of_stock:(@stock.quantity_of_stock - params[:amount][i].to_i)
+                    )
+                # # redirect_to stock_path(@order.goods_master_id)
+    
             end
-
-            # render plain: params.inspect
-
-        # redirect_to root_path
-    end
+                
+        end
 
     end
 
+    
     def edit
     end
 end
