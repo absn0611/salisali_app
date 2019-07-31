d0592d8136bf676b05429f88b1b48f17a6f29d4d
absn0611 <51868578+absn0611@users.noreply.github.com>
2019-07-31 11:44:15 +0900

    commit14

--- salisali/app/controllers/orders_controller.rb
+++ salisali/app/controllers/orders_controller.rb
@@ -1,11 +1,42 @@
 class OrdersController < ApplicationController
 
     def index
+
         if params[:amount].map(&:to_i).sum == 0 or params[:amount].map(&:to_i).sum == nil
-            @orders = params
-            
+
             redirect_to root_path, notice: '注文する商品を指定してください' 
+            return
         end
+
+        @stocks = Stock.all
+        params[:goods_name].count.times do |i| 
+            
+            if params[:amount][i].to_i > @stocks[i].quantity_of_stock
+                @alert = []
+                @alert.push(
+                    [params[:goods_master_id][i],params[:amount][i].to_i,@stocks[i].quantity_of_stock]
+                )
+            end
+            
+            
+        # redirect_to root_path, notice: '注文する商品を指定してください' 
+
+
+        
+        end
+
+        # render plain:
+        #         @alert.inspect
+        #         #  (params[:amount][0].to_i + @stocks[0].quantity_of_stock).inspect
+        #         # # params[:goods_name].count.inspect
+        #         return
+        if 
+            @alert != nil
+            flash[:notice] = '在庫が不足しています'
+            # redirect_to root_path
+            redirect_to root_path(@alert)
+        end
+        
 
     end
     
@@ -14,25 +45,7 @@ class OrdersController < ApplicationController
         if params[:amount].sum == 0 or params[:amount].sum == nil
             redirect_to root_path, notice: '注文する商品を指定してください' 
         else
-            params[:goods_master_id].count.times do |i| 
-                @order = Order.new(
-                    user_id:@current_user.id, 
-                    goods_master_id:params[:goods_master_id][i], 
-                    amount:params[:amount][i],
-                    delivery_date:Time.current.since(delivery_c)
-                    )
-                    
-                @order.save
-
-                # @user = User.find(@current_user.id)
-                # render plain: 
-                # # @current_user.area_master_id
-                # # @current_user
-                # @order.inspect
-                # @user.area_master.distance_from_store
-                # .inspect
-                # return
-        end
+            
 
         params[:goods_master_id].count.times do |i| 
             @stock = Stock.find(params[:goods_master_id][i])
@@ -43,8 +56,21 @@ class OrdersController < ApplicationController
                 # # redirect_to stock_path(@order.goods_master_id)
     
             end
+
+            params[:goods_master_id].count.times do |i| 
+                @order = Order.new(
+                    user_id:@current_user.id, 
+                    goods_master_id:params[:goods_master_id][i], 
+                    amount:params[:amount][i],
+                    delivery_date:Time.current.since(delivery_c)
+                    )
+                    
+                @order.save
+
+        end
                 
         end
+
 
     end
 
@@ -64,6 +90,12 @@ class OrdersController < ApplicationController
                 end
             end
         end
+    end
+
+    private
+
+    def less_stock
+        flash.now[:less_s] = "足りません"
     end
 
 end
