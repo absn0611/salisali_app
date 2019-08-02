b0f7cdec9e536435fadd97281c5de0f35d9ba608
absn0611 <51868578+absn0611@users.noreply.github.com>
2019-08-02 19:49:32 +0900

    commit19

--- salisali/app/controllers/stocks_controller.rb
+++ salisali/app/controllers/stocks_controller.rb
@@ -10,5 +10,23 @@ class StocksController < ApplicationController
   end
 
   def create
+
+    if params[:new]
+      render plain: params.inspect
+      # redirect_to new_stock_path
+    else
+      params[:goods_master_id].count.times do |i| 
+        if params[:amount][i] == ""
+        else
+        @stock = Stock.find(params[:goods_master_id][i])
+            @order = Stock.where(goods_master_id:params[:goods_master_id][i]).update(
+                goods_master_id:params[:goods_master_id][i],
+                quantity_of_stock:(@stock.quantity_of_stock + params[:amount][i].to_i)
+                )
+        end
+      end
+      redirect_to new_stock_path
+    end
+    
   end
 end
--- salisali/db/schema.rb
+++ salisali/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_041242) do
+ActiveRecord::Schema.define(version: 2019_08_02_092944) do
 
   create_table "area_masters", id: :string, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "area_name"
