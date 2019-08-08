4c638ec5ca82d8493dbb9c10f8536736d629b282
absn0611 <51868578+absn0611@users.noreply.github.com>
2019-08-08 12:03:06 +0900

    commit29
    管理者メニューにユーザー情報一覧、編集、削除機能を追加しました

--- salisali/app/controllers/users_controller.rb
+++ salisali/app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   skip_before_action :require_sign_in!, only: [:new, :create]
 
+  def index
+    @users = User.all.order(created_at:'asc')
+  end
+
   def new
     @user = User.new
   end
@@ -12,6 +16,28 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+    # render plain: params.inspect
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(@current_user.id)
+  end
+
+  def update
+    # render plain: params[:id].inspect
+    @user = User.update(user_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+    # render plain: @user.inspect
   end
 
   private
