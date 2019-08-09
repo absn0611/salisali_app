99f365bb88cc273458cbcb7bde2bfdd863d5731f
absn0611 <51868578+absn0611@users.noreply.github.com>
2019-08-09 18:49:06 +0900

    commit41

--- salisali/app/controllers/area_masters_controller.rb
+++ salisali/app/controllers/area_masters_controller.rb
@@ -25,21 +25,42 @@ class AreaMastersController < ApplicationController
 
     def new
         admin_login
+        if params[:id] !="" and params[:area_name] != "" and params[:distance_from_store].to_i > 0 and AreaMaster.find_by(id: params[:id]) == nil and AreaMaster.find_by(area_name: params[:area_name]) == nil
+      
+            render "area_masters/new"
+            return
+    
+        else
+
+        if params[:id] == ""
+            flash.now[:notice] = "エリアコードを指定してください"
+        end
+  
+        if params[:area_name] == ""
+          flash.now[:notice2] = "エリア名を指定してください"
+        end
+  
+        if params[:distance_from_store].to_i <= 0
+            flash.now[:notice3] = "倉庫からの距離を０以上の値で指定してください"
+        end
+
         if AreaMaster.where(id:params[:id]).any?
             @area_masters = AreaMaster.all
 
             flash.now[:notice] = params[:id] + "は既に登録されています"
-            render "index"
-        return
         end
 
         if AreaMaster.where(area_name:params[:area_name]).any?
             @area_masters = AreaMaster.all
 
             flash.now[:notice] = params[:area_name] + "は既に登録されています"
-            render "index"
-        return
         end
+        @area_masters = AreaMaster.all
+
+        render "index"
+        return
+
+    end
     
     
         # render plain: params.inspect
